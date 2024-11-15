38b77d05c890081b3383849e24dbe56b0ab5653a
Vidinpp <Vidinpp0@gamil.com>
2024-11-15 18:08:27 +0530

    Printing Path

--- vidin.sh
+++ vidin.sh
@@ -1,2 +1,12 @@
 #! /bin/bash	
-echo $(pwd)
+read -p "please enter the path :" r1
+
+ if [-f "$r1"]; then
+	echo"$r1 is a file"
+ elif [-d "$r1"]; then
+	 echo "$r1 is a driectory"
+ else 
+	 echo "$r1 is a something else"
+  fi
+
+
